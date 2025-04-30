6793bbeb52956c7d2404c87d22d284a4fc188785
Pedro Tramontini <pedroh.tramontini@gmail.com>
2025-04-30 16:54:09 -0300

    Adição do componente Container e Banner, além de passar a imagem do fundo do banner dinamicamente e algumas alterações no css do do header/footer/container/banner

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,17 @@
+import Banner from "./components/Banner";
+import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
-        <Header />
-    <h1> Hello World </h1>
+    <Header />
+    <Banner image="home"/>
+    <Container>
+    <h1>Hello Word</h1>
+    <p>Estou aprendendo React</p>
+    </Container>
     <Footer />
   </>
   );
