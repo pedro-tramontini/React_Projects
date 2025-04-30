2c6f69e5871b63aa00e0fef6a322e25ba556285b
Pedro Tramontini <pedroh.tramontini@gmail.com>
2025-04-30 15:01:23 -0300

    Primeiro programa em React

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+        <Header />
+    <h1> Hello World </h1>
+    <Footer />
+  </>
+  );
+}
+export default App;
---
+++ my-app/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import styles from "./Footer.module.css"
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <h2>MaiaFlix &copy; Desenvolvido por Pedro Tramontini 2025</h2>
+        </footer>
+
+    );
+}
+
+export default Footer;
