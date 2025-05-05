6688c8e1e24c04555cda26982bbbcd65bd893471
Pedro Tramontini <pedroh.tramontini@gmail.com>
2025-05-05 13:47:06 -0300

    Controle de escopo do App.js, alocando corretamente o categories e a função filterCategory

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,9 @@
 import Banner from "./components/Banner";
 import Card from "./components/Card";
-import Category from "./components/Category";
+import Category, {categories, filterCategory} from "./components/Category";
 import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import videos from "./json/videos.json"
-
-const categories = [
-  "Geografia",
-  "Como fazer e usar",
-  "Astronomia e Geografia",
-  "Climatologia, Meteorologia, Vegetação",
-  "Geologia e Hidrografia"
-]
-
-// Essa função percorre o json videos retornando se o elemento da lista no inidice (id) é idêntigo ao titulo da constante categories
-  function filterCategory(id) {
-    return videos.filter( video => video.category === categories[id]);
-  }
 
 function App() {
   return (
@@ -26,22 +12,12 @@ function App() {
     <Banner image="home"/>
     <Container>
 
-{/*O filter category retorna a lista json igual ao titulo de categories, e usando um mapeamento do json, coloca os elementos dentro dos carts */}
-    <Category category={categories[0]}>
-      {filterCategory(0).map((video) => <Card id={video.id} key={video.id}/>)}
+    
+    {categories.map((category, index) =>
+    <Category category={category}>
+      {filterCategory(index).map((video) => <Card id={video.id} key={video.id}/>)}
     </Category>
-    <Category category={categories[1]}>
-      {filterCategory(1).map((video) => <Card id={video.id} key={video.id}/>)}
-    </Category>
-    <Category category={categories[2]}>
-      {filterCategory(2).map((video) => <Card id={video.id} key={video.id}/>)}
-    </Category>
-    <Category category={categories[3]}>
-      {filterCategory(3).map((video) => <Card id={video.id} key={video.id}/>)}
-    </Category>
-    <Category category={categories[4]}>
-      {filterCategory(4).map((video) => <Card id={video.id} key={video.id}/>)}
-    </Category>
+  )}
 
     </Container>
     <Footer />
