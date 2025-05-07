6a5bc3c6de9969d876eb0d5a4a7ff877583b3b01
Pedro Tramontini <pedroh.tramontini@gmail.com>
2025-05-07 08:51:17 -0300

    Rotas dinâmicas para acessar os vídeos listados

--- my-app/src/pages/Watch/index.js
+++ my-app/src/pages/Watch/index.js
@@ -1,16 +1,31 @@
+import { useParams } from "react-router-dom"
 import Container from "../../components/Container"
 import Footer from "../../components/Footer"
 import Header from "../../components/Header"
 import styles from "./Watch.module.css"
+import videos from "../../json/videos.json"
+import PageNotFound from "../PageNotFound"
 
 function Watch() {
+    const params = useParams();
+    const video = videos.find((video) => { return video.id === params.id })
+    if (!video) {return <PageNotFound />};
+
     return(
         <>
         <Header />
         <Container>
             <section className={styles.watch}>
-                <h1>Assistir</h1>
-                <iframe width="854" height="482" src="https://www.youtube.com/embed/2y1qW_aH0gQ?list=PLnex8IkmReXyNgtea8ly0Pn7Kk7Kavn9v" title="Curso de React JS Aula 08 Criar Rotas e Páginas, React Router DOM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <iframe 
+                    width="854" 
+                    height="482" 
+                    src={`https://www.youtube.com/embed/${video.id}`} 
+                    title="Curso de React JS Aula 08 Criar Rotas e Páginas, React Router DOM" 
+                    frameborder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                    referrerpolicy="strict-origin-when-cross-origin" 
+                    allowfullscreen>
+                </iframe>
             </section>
         </Container>
         <Footer />
